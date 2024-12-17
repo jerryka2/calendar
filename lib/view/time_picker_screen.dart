eafc4d21018f7a8982f14ef4e3f7243f855d4846
Suman <sumaka37@gmail.com>
2024-12-17 21:14:00 +0545

    change in code

--- lib/view/time_picker_screen.dart
+++ lib/view/time_picker_screen.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TimePickerWidget extends StatefulWidget {
-  final Function(TimeOfDay) onTimeSelected;
+class TimePickerWidget extends StatelessWidget {
+  final ValueNotifier<TimeOfDay> timeNotifier;
 
-  const TimePickerWidget({super.key, required this.onTimeSelected});
+  const TimePickerWidget({required this.timeNotifier, super.key});
 
-  @override
-  // ignore: library_private_types_in_public_api
-  _TimePickerWidgetState createState() => _TimePickerWidgetState();
-}
-
-class _TimePickerWidgetState extends State<TimePickerWidget> {
-  TimeOfDay _selectedTime = TimeOfDay.now();
-
-  Future<void> _pickTime() async {
-    TimeOfDay? pickedTime = await showTimePicker(
+  void _pickTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
       context: context,
-      initialTime: _selectedTime,
+      initialTime: timeNotifier.value,
     );
 
     if (pickedTime != null) {
-      setState(() {
-        _selectedTime = pickedTime;
-      });
-      widget.onTimeSelected(_selectedTime);
+      timeNotifier.value = pickedTime;
     }
   }
 
@@ -31,15 +20,20 @@ class _TimePickerWidgetState extends State<TimePickerWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          "Time: ${_selectedTime.format(context)}",
-          style: TextStyle(fontSize: 20),
+        ValueListenableBuilder<TimeOfDay>(
+          valueListenable: timeNotifier,
+          builder: (context, time, _) {
+            return Text(
+              "Time: ${time.format(context)}",
+              style: const TextStyle(fontSize: 20),
+            );
+          },
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         ElevatedButton(
-          onPressed: _pickTime,
+          onPressed: () => _pickTime(context),
           style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-          child: Text("Change Time"),
+          child: const Text("Change Time"),
         ),
       ],
     );
