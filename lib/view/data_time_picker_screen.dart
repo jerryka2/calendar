93650e6036b6a85ea456d6752d3f5f7984586153
Suman <sumaka37@gmail.com>
2024-12-17 21:15:20 +0545

    another

--- lib/view/data_time_picker_screen.dart
+++ lib/view/data_time_picker_screen.dart
@@ -2,45 +2,26 @@ import 'package:calendar/view/data_picker_screen.dart';
 import 'package:calendar/view/time_picker_screen.dart';
 import 'package:flutter/material.dart';
 
-class DateTimePickerScreen extends StatefulWidget {
-  const DateTimePickerScreen({super.key});
+class DateTimePickerScreen extends StatelessWidget {
+  final ValueNotifier<TimeOfDay> _timeNotifier = ValueNotifier(TimeOfDay.now());
+  final ValueNotifier<DateTime> _dateNotifier = ValueNotifier(DateTime.now());
 
-  @override
-  _DateTimePickerScreenState createState() => _DateTimePickerScreenState();
-}
-
-class _DateTimePickerScreenState extends State<DateTimePickerScreen> {
-  // ignore: unused_field
-  TimeOfDay _currentTime = TimeOfDay.now();
-  // ignore: unused_field
-  DateTime _currentDate = DateTime.now();
-
-  void _updateTime(TimeOfDay newTime) {
-    setState(() {
-      _currentTime = newTime;
-    });
-  }
-
-  void _updateDate(DateTime newDate) {
-    setState(() {
-      _currentDate = newDate;
-    });
-  }
+  DateTimePickerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Time & Date Picker'),
+        title: const Text('Time & Date Picker'),
         backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TimePickerWidget(onTimeSelected: _updateTime),
-            SizedBox(height: 20),
-            DatePickerWidget(onDateSelected: _updateDate),
+            TimePickerWidget(timeNotifier: _timeNotifier),
+            const SizedBox(height: 20),
+            DatePickerWidget(dateNotifier: _dateNotifier),
           ],
         ),
       ),
